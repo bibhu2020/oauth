b3653fe5500e5ad9361e63dc7b4b28bda12ef1cb
Bibhu Mishra <v-bimishra@outlook.com>
2025-07-20 07:33:17 -0500

    updated lint

--- oauthapi/eslint.config.js
+++ oauthapi/eslint.config.js
@@ -1,19 +1,17 @@
 import js from "@eslint/js";
 import tseslint from "typescript-eslint";
 
-export default [
-  js.configs.recommended,
-  tseslint.configs.recommended,
-  {
-    files: ["**/*.js", "**/*.ts"],
-    rules: {
-      "no-unused-vars": "warn",
-      "no-console": "warn",
-      "eqeqeq": "error",
-      "curly": "error",
-      "semi": ["error", "always"],
-      "quotes": ["error", "single"],
-      "indent": ["error", 2],
-    },
+export default {
+  ...js.configs.recommended,
+  ...tseslint.configs.recommended,
+  files: ["**/*.js", "**/*.ts"],
+  rules: {
+    "no-unused-vars": "warn",
+    "no-console": "warn",
+    "eqeqeq": "error",
+    "curly": "error",
+    "semi": ["error", "always"],
+    "quotes": ["error", "single"],
+    "indent": ["error", 2],
   },
-];
+};
